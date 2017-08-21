d69a4291ad11f3bcde2ef347528aacd178870b0d
Julius Eerola <eerolaj1@niksula.hut.fi>
2017-08-21 23:35:53 +0300

    created platform for unit tests

---
+++ EssiTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EssiTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EssiTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3e742641-c955-465c-9883-7bd0ba4f5259")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EssiTests/RequestControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Essi.Controllers;
+using Essi.Models;
+using System.Web.Mvc;
+
+namespace EssiTests
+{
+    [TestClass]
+    public class RequestControllerTest
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        [TestMethod]
+        public async void TestEmptyRequestIndexView()
+        {
+            var controller = new RequestController();
+            ViewResult result = await controller.Index() as ViewResult;
+            Assert.IsNotNull(result);
+        }
+    }
+}
